5b47fb95785b6742dd1c0967ac77882b13dc7077
CaolanCode <cmacmahon3@gmail.com>
2023-07-14 17:08:28 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    colors: {
+      "primary" : "#007bff",
+      "secondary" : "#1985c5",
+      "accent" : "#8c9eff",
+      "neutral" : "#444"
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  },
+  plugins: [],
+}
