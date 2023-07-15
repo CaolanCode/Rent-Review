26e798b51199a3d6ceed289015942594106a717c
CaolanCode <cmacmahon3@gmail.com>
2023-07-15 13:19:31 +0100

    add username input for signup

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -4,17 +4,17 @@ import Link from "next/link"
 export default function Login() {
   return (
     <div className="flex items-center justify-center h-[90vh] pb-[5vh]">
-      <form action="" className="flex flex-col gap-4 items-center">
+      <form action="" className="flex flex-col gap-4 items-center w-64">
         <h1 className="text-white font-bold text-2xl pb-2">Login</h1>
         <input 
           type="text" 
-          placeholder="Username/email" 
-          className="px-4 py-1 rounded-lg" 
+          placeholder="Email" 
+          className="px-4 py-1 rounded-lg w-full" 
         />
         <input 
           type="password" 
           placeholder="Password" 
-          className="px-4 py-1 rounded-lg"
+          className="px-4 py-1 rounded-lg w-full"
         />
         <div className="flex items-center gap-8">
           <Button 
--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -4,22 +4,27 @@ import Link from "next/link"
 export default function SignUp() {
   return (
     <div className="flex items-center justify-center h-[90vh] pb-[5vh]">
-      <form action="" className="flex flex-col gap-4 items-center">
+      <form action="" className="flex flex-col gap-4 items-center w-64">
         <h1 className="text-white font-bold text-2xl pb-2">Sign Up</h1>
         <input 
           type="text" 
-          placeholder="Username/email" 
-          className="px-4 py-1 rounded-lg focus:outline-none" 
+          placeholder="Username" 
+          className="px-4 py-1 rounded-lg focus:outline-none w-full" 
+        />
+        <input 
+          type="text" 
+          placeholder="Email" 
+          className="px-4 py-1 rounded-lg focus:outline-none w-full" 
         />
         <input 
           type="password" 
           placeholder="Password" 
-          className="px-4 py-1 rounded-lg focus:outline-none"
+          className="px-4 py-1 rounded-lg focus:outline-none w-full"
         />
         <input 
           type="password" 
           placeholder="Password" 
-          className="px-4 py-1 rounded-lg focus:outline-none"
+          className="px-4 py-1 rounded-lg focus:outline-none w-full"
         />
         <div className="flex items-center gap-8">
           <Button 
