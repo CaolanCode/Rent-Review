dded0e7cd2d1d0eab47d2ef36afbd1abe106f25e
CaolanCode <cmacmahon3@gmail.com>
2023-07-18 13:13:23 +0100

    add title to search

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -2,7 +2,8 @@ import SearchInput from "./search-input"
 
 export default function SearchPage() {
   return (
-    <div className="flex justify-center items-center h-[90vh]">
+    <div className="flex justify-center items-center h-[90vh] flex-col gap-2 pb-[5vh]">
+      <h1 className="text-2xl text-white font-bold">Search a Property</h1>
       <SearchInput />
     </div>
   )
