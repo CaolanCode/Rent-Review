35f1941b4b84be73764f6d45cec5ac5f217ff44a
CaolanCode <cmacmahon3@gmail.com>
2023-07-18 12:23:53 +0100

    add use client for search bar

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,12 +1,9 @@
+import SearchInput from "./search-input"
 
 export default function SearchPage() {
   return (
     <div className="flex justify-center items-center h-[90vh]">
-      <input 
-        type="text" 
-        className="px-10 py-3 border-accent border-2 rounded-3xl text-center focus:outline-none" 
-        placeholder="Zip-Code"
-      />
+      <SearchInput />
     </div>
   )
 }
---
+++ src/app/search/search-input.js
@@ -0,0 +1,11 @@
+"use client"
+
+export default function SearchInput() {
+  return (
+    <input 
+      type="text" 
+      className="px-10 py-3 border-accent border-2 rounded-3xl text-center focus:outline-none" 
+      placeholder="Zip-Code"
+    />
+  )
+}
