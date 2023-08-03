24621de999a32ca671228442777ce721d51a3b77
CaolanCode <cmacmahon3@gmail.com>
2023-08-03 22:52:38 +0100

    tidy form button class

--- src/app/components/FormButtons.tsx
+++ src/app/components/FormButtons.tsx
@@ -3,16 +3,17 @@ interface FormProp {
 }
 
 export default function FormButtons({name}: FormProp) {
+  const classList = "text-lg border py-1 px-2 rounded-md border-accent text-primary cursor-pointer hover:bg-neutral" 
   return (
     <div className="flex items-center gap-8">
       <button 
         type="submit" 
-        className="text-lg border py-1 px-2 rounded-md border-accent text-primary cursor-pointer hover:bg-neutral">
+        className={classList}>
         {name}
       </button>
       <button 
         type="reset" 
-        className="text-lg border py-1 px-2 rounded-md border-accent text-primary cursor-pointer hover:bg-neutral">
+        className={classList}>
         Cancel
       </button>
     </div>
--- src/app/components/ui/LinkBtn.tsx
+++ src/app/components/ui/LinkBtn.tsx
@@ -6,7 +6,6 @@ interface LinkBtnProps {
 }
 
 export default function LinkBtn({path, name}: LinkBtnProps) {
-
   return (
     <div className="text-lg border py-1 px-2 rounded-md border-accent text-primary cursor-pointer hover:bg-neutral" >
       <Link href={path}>
