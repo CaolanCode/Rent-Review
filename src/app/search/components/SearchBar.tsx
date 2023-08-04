c058aeeea083822a3512d5052d5c5a270fdcc685
CaolanCode <cmacmahon3@gmail.com>
2023-08-04 11:58:38 +0100

    add search button

--- src/app/search/components/SearchBar.tsx
+++ src/app/search/components/SearchBar.tsx
@@ -1,11 +1,19 @@
 "use client"
 
+import { FaSearchLocation } from "react-icons/fa"
+
 export default function SearchBar() {
   return (
-    <input 
-      type="text" 
-      className="px-10 py-3 border-accent border-2 rounded-3xl text-center focus:outline-none" 
-      placeholder="Zip-Code"
-    />
+    <div className="px-10 py-3 border-accent border-2 rounded-3xl text-center bg-white">
+      <input 
+        type="text" 
+        className="focus:outline-none"
+        placeholder="Zip-Code"
+      />
+      <button 
+        className="hover:scale-125 cursor-pointer active:scale-100">
+        <FaSearchLocation />
+      </button>
+    </div>
   )
 }
